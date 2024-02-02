729c78807cca1122a0373218cdd8376a586ad7d8
Pravin-Kumar-21 <pravinkumar2108@gmail.com>
2024-02-02 21:24:48 +0530

    photocontainer and slide animation

--- portfolio/views.py
+++ portfolio/views.py
@@ -7,5 +7,4 @@ from django.apps import AppConfig
 # Create your views here.
 def user_detail(request):
     all_users = UserDetails.objects.all()
-    print((all_users))
     return render(request, "home/home.html", context={"all_users": all_users})
