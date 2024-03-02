f2166980c43d3f72362e988f8b2284e4ef960f09
Pravin-Kumar-21 <pravinkumar2108@gmail.com>
2024-03-02 15:42:27 +0530

    nav-bar fixed according to the need

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,3 +1,4 @@
+from typing import Any
 from django.views.generic import ListView
 from django.utils import timezone
 from .models import UserDetails
@@ -5,10 +6,20 @@ from .models import UserDetails
 
 class UserHome(ListView):
     model = UserDetails
-    template_name = "portfolio/userdetails_list.html"  # Adjust the path based on your project structure
+    template_name = "portfolio/userdetails_list.html"
     context_object_name = "User"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["ist_time"] = timezone.now().astimezone(timezone.get_current_timezone())
+        return context
+
+
+class Navbar(ListView):
+    model = UserDetails
+    template_name = "portfolio/nav.html"
+    context_object_name = "User"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
         return context
